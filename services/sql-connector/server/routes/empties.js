e4625d91a136558b2e5ac91bc08b041adcbd7cb9
Patrik Macák <299269037+patrikmacak93@users.noreply.github.com>
2026-08-27 12:42:30 +0200

    Update empties.js

--- services/sql-connector/server/routes/empties.js
+++ services/sql-connector/server/routes/empties.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router ();
-const { getProjects, getEmpties, createEmpty, updateEmpty } = require('../controllers/emptiesController');
+const { getProjects, getEmpties, createEmpty, updateEmpty, deleteEmpty } = require('../controllers/emptiesController');
 
 router.get('/empties', getEmpties);
 router.post('/empties', createEmpty);
 router.put('/empties/:id', updateEmpty)
+router.delete('/empties/:id', deleteEmpty)
 
 module.exports = router
